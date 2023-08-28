c6df3d585d2b23b4bda6a1a72c65b5508294776b
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-28 16:02:47 -0500

    make name, email, password Zod required for signup

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -24,9 +24,9 @@ const sendAuthResponse = (user: UserDoc) => {
 const signup = publicProcedure
   .input(
     z.object({
-      name: z.string().optional(),
-      email: z.string().optional(),
-      password: z.string().optional(),
+      name: z.string(),
+      email: z.string(),
+      password: z.string(),
       passwordConfirm: z.string().optional(),
       avatar: z.string().optional(),
       netWorth: z.number().optional(),
