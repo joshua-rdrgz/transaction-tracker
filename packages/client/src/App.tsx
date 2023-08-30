1379052af0b2eabfd6be8836e8a05cd43c63c923
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-08-30 16:43:01 -0500

    Allow cookies to be sent cross-origin

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -17,6 +17,16 @@ const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
       url: 'http://localhost:3000/api/v1/trpc',
+      /**
+       * Allows cookies to send cross-origin
+       * See: https://trpc.io/docs/client/cors
+       */
+      fetch(url, options) {
+        return fetch(url, {
+          ...options,
+          credentials: 'include',
+        });
+      },
     }),
   ],
 });
