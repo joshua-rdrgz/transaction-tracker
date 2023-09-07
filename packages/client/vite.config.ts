2c12050a46ea540b1a1ae9a590e168a4cfb8d3a8
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-07 16:31:16 -0500

    configure env variables for server base URL

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { config } from 'dotenv';
+
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: process.cwd(),
+  define: {
+    'process.env.SERVER_BASE_URL': JSON.stringify(process.env.SERVER_BASE_URL),
+  },
   plugins: [react(), tsconfigPaths()],
   build: {
     commonjsOptions: {
