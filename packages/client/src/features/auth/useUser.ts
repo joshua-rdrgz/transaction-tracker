90596ad06bd05ec5db4207efee0e94e450001cd3
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-10 02:36:00 -0500

    Get rid of unnecessary error definition

--- packages/client/src/features/auth/useUser.ts
+++ packages/client/src/features/auth/useUser.ts
@@ -1,12 +1,11 @@
 import { trpc } from '@/config/trpc';
 
 export const useUser = () => {
-  const { isLoading, data: response, error } = trpc.getUser.useQuery();
+  const { isLoading, data: response } = trpc.getCurrentUser.useQuery();
 
   return {
     isLoadingUser: isLoading,
     user: response?.data?.user,
     isAuthenticated: !!response?.data?.user,
-    error,
   };
 };
