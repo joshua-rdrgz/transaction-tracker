dfa05bee66abf706a673d57355930aabe67dd4c7
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-10 02:30:12 -0500

    add waitToFinishViaPromise util function

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function waitToFinishViaPromise(callback: () => void) {
+  await new Promise((res) => {
+    callback();
+    res(null);
+  });
+};
