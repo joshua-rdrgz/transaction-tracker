e0e3192fa2925e201033d387e73e47ce4309abe4
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-18 00:26:02 -0500

    Add prismaUser to prisma clients

--- packages/api-server/src/config/prisma.ts
+++ packages/api-server/src/config/prisma.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import PrismaUser from '@/models/userModel';
 
 declare global {
   var prisma: PrismaClient | undefined;
+  var prismaUser: PrismaUser | undefined;
 }
 
 const prisma = globalThis.prisma || new PrismaClient();
 
+export const prismaUser = globalThis.prismaUser || new PrismaUser(prisma.user);
+
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prismaUser = prismaUser;
 
 export default prisma;
