1c50ab36d27cce0159deafb2e18c0fd41b859229
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-19 13:32:47 -0500

    onSuccess, navigate to homepage

--- packages/client/src/features/auth/useLogin.ts
+++ packages/client/src/features/auth/useLogin.ts
@@ -1,7 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import { trpc } from '@/config/trpc';
 
 export const useLogin = () => {
-  const { isLoading, mutate } = trpc.login.useMutation();
+  const navigate = useNavigate();
+  const { isLoading, mutate } = trpc.login.useMutation({
+    onSuccess: () => navigate('/', { replace: true }),
+  });
 
   return {
     isLoggingIn: isLoading,
