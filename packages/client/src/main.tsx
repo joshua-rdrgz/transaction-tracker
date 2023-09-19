71c79fcd6006af215f050bbc898943ac793045ed
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-19 18:29:43 -0500

    Re-add ErrorBoundary for 404 errors

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import App from './App.tsx';
+
+import ErrorPage from '@/pages/Error.tsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ErrorBoundary
+    FallbackComponent={ErrorPage}
+    onReset={() => window.location.replace('/login')}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ErrorBoundary>
 );
