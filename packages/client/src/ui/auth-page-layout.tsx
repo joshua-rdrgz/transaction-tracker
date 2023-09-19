5953143c91c60d5df4d7b91bf6b538b46034693a
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-19 18:35:29 -0500

    Switch colors to CSS Variables

--- packages/client/src/ui/auth-page-layout.tsx
+++ packages/client/src/ui/auth-page-layout.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { Logo } from '@/ui/logo';
+import { Button } from './button';
 
 interface IAuthPageLayoutProps {
   formTitle: string;
@@ -18,17 +20,15 @@ export const AuthPageLayout: React.FC<IAuthPageLayoutProps> = ({
   footerLinksTo,
 }) => {
   return (
-    <div className='flex flex-col justify-between min-h-screen bg-gray-200 text-gray-900 py-10 px-7 text-center'>
+    <div className='flex flex-col justify-between min-h-screen py-10 px-7 text-center bg-secondary'>
       <header className='text-4xl'>
-        <figure className='w-1/3 mx-auto'>
-          <img src='/logo-color.svg' className='w-full' />
-        </figure>
+        <Logo figureClassName='w-1/3' />
       </header>
       <main>
         <div className='flex flex-col gap-4'>
-          <h1 className='font-display text-5xl text-orange-950'>{formTitle}</h1>
-          <p className='text-orange-900'>{formSubtitle}</p>
-          <div className='w-full mx-auto bg-gray-50 rounded-md py-5 px-6 sm:w-3/4 md:w-1/2 xl:w-1/3'>
+          <h1 className='font-display text-5xl text-primary'>{formTitle}</h1>
+          <p className='text-primary'>{formSubtitle}</p>
+          <div className='w-full mx-auto bg-background rounded-md py-5 px-6 sm:w-3/4 md:w-1/2 xl:w-1/3'>
             {formComponent}
           </div>
         </div>
@@ -36,7 +36,9 @@ export const AuthPageLayout: React.FC<IAuthPageLayoutProps> = ({
       <footer className=''>
         {footerLabel}{' '}
         <Link to={footerLinksTo}>
-          <span className='text-orange-950 underline'>{footerLinkLabel}</span>
+          <Button variant='link' className='text-md'>
+            {footerLinkLabel}
+          </Button>
         </Link>
       </footer>
     </div>
