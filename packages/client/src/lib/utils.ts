f81aa6b1a5967e638926b6472bc6cdbfd7858a1c
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-19 13:32:06 -0500

    delete waitToFinishViaPromise

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -4,10 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export async function waitToFinishViaPromise(callback: () => void) {
-  await new Promise((res) => {
-    callback();
-    res(null);
-  });
-};
