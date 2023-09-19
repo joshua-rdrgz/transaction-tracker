0bcabcff118e9c25441a6ea012eae95a8af85d00
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-19 13:31:17 -0500

    Add navigation to dashboard onSuccess

--- packages/client/src/features/auth/useSignup.ts
+++ packages/client/src/features/auth/useSignup.ts
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
 import { trpc } from '@/config/trpc';
 import { toast } from 'react-hot-toast';
 
 export const useSignup = () => {
+  const navigate = useNavigate();
+
   const { isLoading, mutate } = trpc.signup.useMutation({
     onSuccess: (_, variables) => {
+      navigate('/', { replace: true });
       toast.success(`Successfully signed up.  Welcome, ${variables.name}!`);
     },
   });
