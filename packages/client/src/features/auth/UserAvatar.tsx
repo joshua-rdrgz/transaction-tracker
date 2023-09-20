9a33c77c99572e1ea67597ce9f30a5535149d8a1
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-19 19:04:08 -0500

    Add alt text to User Avatar image

--- packages/client/src/features/auth/UserAvatar.tsx
+++ packages/client/src/features/auth/UserAvatar.tsx
@@ -6,7 +6,11 @@ export const UserAvatar = () => {
   return (
     <div className='flex gap-2 items-center'>
       <figure className='w-9'>
-        <img src={user?.avatar || '/default-user.png'} className='w-full' />
+        <img
+          src={user?.avatar || '/default-user.png'}
+          alt='User Avatar'
+          className='w-full'
+        />
       </figure>
       <span className='text-sm'>{user?.name}</span>
     </div>
