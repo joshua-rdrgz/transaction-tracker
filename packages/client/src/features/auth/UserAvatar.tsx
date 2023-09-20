b6340316d4928a0efebe0f6442896ea7ab2d861e
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-19 19:03:09 -0500

    Create UserAvatar component

---
+++ packages/client/src/features/auth/UserAvatar.tsx
@@ -0,0 +1,14 @@
+import { useUser } from '@/features/auth/useUser';
+
+export const UserAvatar = () => {
+  const { user } = useUser();
+
+  return (
+    <div className='flex gap-2 items-center'>
+      <figure className='w-9'>
+        <img src={user?.avatar || '/default-user.png'} className='w-full' />
+      </figure>
+      <span className='text-sm'>{user?.name}</span>
+    </div>
+  );
+};
