10a12f648b2ab4e848083cae8e7911da82a0051d
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-27 18:26:01 -0500

    Implement real avatar from s3 bucket

--- packages/client/src/features/auth/UserAvatar.tsx
+++ packages/client/src/features/auth/UserAvatar.tsx
@@ -7,9 +7,14 @@ export const UserAvatar = () => {
     <div className='flex gap-2 items-center'>
       <figure className='w-9'>
         <img
-          src={user?.avatar || '/default-user.png'}
+          src={
+            user?.avatar
+              ? `${process.env.SERVER_BASE_URL}${user.avatar}` ||
+                '/default-user.png'
+              : '/default-user.png'
+          }
           alt='User Avatar'
-          className='w-full'
+          className='w-full rounded-full'
         />
       </figure>
       <span className='text-sm'>{user?.name}</span>
