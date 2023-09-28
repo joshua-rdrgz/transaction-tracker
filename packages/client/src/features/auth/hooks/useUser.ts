81b4153919929fd39f3882f4ff704e71ae7635b7
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-28 16:33:28 -0500

    Update useUser to use Loader

--- packages/client/src/features/auth/hooks/useUser.ts
+++ packages/client/src/features/auth/hooks/useUser.ts
@@ -1,11 +1,25 @@
+import { useLoaderData } from 'react-router-dom';
 import { trpc } from '@/config/trpc';
+import { currentUserLoader } from '@/features/auth/fetchers/currentUser';
+import { ReturnTypeLoader } from '@/lib/utils';
 
 export const useUser = () => {
-  const { isLoading, data: response } = trpc.getCurrentUser.useQuery();
+  const initialData = useLoaderData() as ReturnTypeLoader<
+    typeof currentUserLoader
+  >;
+
+  const {
+    isLoading,
+    data: {
+      data: { user },
+    },
+  } = trpc.getCurrentUser.useQuery(undefined, {
+    initialData,
+  });
 
   return {
     isLoadingUser: isLoading,
-    user: response?.data?.user,
-    isAuthenticated: !!response?.data?.user,
+    user,
+    isAuthenticated: !!user,
   };
 };
