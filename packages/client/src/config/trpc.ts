198d1bf20ba575ac108f0fe7900d10de6b12f3fa
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-27 23:36:26 -0500

    abstract trpcClient, add vanillaClient

--- packages/client/src/config/trpc.ts
+++ packages/client/src/config/trpc.ts
@@ -1,4 +1,46 @@
-import { createTRPCReact } from '@trpc/react-query';
+import {
+  createTRPCProxyClient,
+  createTRPCReact,
+  httpBatchLink,
+} from '@trpc/react-query';
 import { AppRouter } from 'api-server';
 
 export const trpc = createTRPCReact<AppRouter>();
+
+export const trpcReactClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/api/v1/trpc',
+      /**
+       * Allows cookies to send cross-origin
+       * See: https://trpc.io/docs/client/cors
+       */
+      fetch(url, options) {
+        return fetch(url, {
+          ...options,
+          credentials: 'include',
+        });
+      },
+    }),
+  ],
+});
+
+export const trpcVanillaClient = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/api/v1/trpc',
+      /**
+       * Allows cookies to send cross-origin
+       * See: https://trpc.io/docs/client/cors
+       */
+      fetch(url, options) {
+        return fetch(url, {
+          ...options,
+          credentials: 'include',
+        });
+      },
+    }),
+  ],
+});
+
+export const TRPCProvider = trpc.Provider;
