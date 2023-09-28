bab353cbeb88dd36ac7d8590632bc5f8f2555b10
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-27 23:38:20 -0500

    update App

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -6,64 +6,28 @@ import {
   RouterProvider,
   Outlet,
 } from 'react-router-dom';
-import {
-  MutationCache,
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster, toast } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 import { ThemeProvider } from '@/features/theme/theme-provider';
 import { ErrorBoundary } from 'react-error-boundary';
 
-import { trpc } from '@/config/trpc';
+import { queryClient } from '@/config/queryClient';
+import { trpcReactClient, TRPCProvider } from '@/config/trpc';
+
+import { readAccountsLoader } from '@/features/accounts/fetchers/readAccounts';
 
 import { ProtectedRoute } from '@/features/auth/ProtectedRoute';
 import { AppLayout } from '@/ui/app-layout';
 
 import Signup from '@/pages/Signup';
 import Login from '@/pages/Login';
-import Settings from '@/pages/Settings';
+import AccountsPage from '@/pages/Accounts';
+import SettingsPage from '@/pages/Settings';
 import ErrorPage from '@/pages/Error';
 
 import './index.css';
-
-const onError = (error: any) => {
-  toast.error(`Something went wrong: ${error.message}`);
-};
-
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({ onError }),
-  mutationCache: new MutationCache({ onError }),
-  defaultOptions: {
-    queries: {
-      staleTime: 0,
-      retry: false,
-      useErrorBoundary: true,
-    },
-  },
-});
-
-const trpcClient = trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: 'http://localhost:3000/api/v1/trpc',
-      /**
-       * Allows cookies to send cross-origin
-       * See: https://trpc.io/docs/client/cors
-       */
-      fetch(url, options) {
-        return fetch(url, {
-          ...options,
-          credentials: 'include',
-        });
-      },
-    }),
-  ],
-});
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -87,10 +51,14 @@ const router = createBrowserRouter(
       >
         <Route index element={<Navigate replace to='/dashboard' />} />
         <Route path='dashboard' element={<div>Dashboard / Home</div>} />
-        <Route path='accounts' element={<div>Accounts</div>} />
+        <Route
+          path='accounts'
+          loader={readAccountsLoader(queryClient)}
+          element={<AccountsPage />}
+        />
         <Route path='budget' element={<div>Budget (Year)</div>} />
         <Route path='budget/:monthId' element={<div>Budget (Month)</div>} />
-        <Route path='settings' element={<Settings />} />
+        <Route path='settings' element={<SettingsPage />} />
       </Route>
 
       {/* PUBLIC ROUTES */}
@@ -103,13 +71,13 @@ const router = createBrowserRouter(
 function App() {
   return (
     <ThemeProvider>
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <TRPCProvider client={trpcReactClient} queryClient={queryClient}>
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
           <Toaster position='top-center' />
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
-      </trpc.Provider>
+      </TRPCProvider>
     </ThemeProvider>
   );
 }
