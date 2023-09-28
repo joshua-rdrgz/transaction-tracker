56f8d89fe64e1f8c9a490fdfb12218052648fa4b
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-27 20:41:28 -0500

    Add logout button to header

--- packages/client/src/ui/header.tsx
+++ packages/client/src/ui/header.tsx
@@ -1,11 +1,15 @@
 import { ModeToggle } from '@/features/theme/ModeToggle';
 import { UserAvatar } from '@/features/auth/UserAvatar';
+import { Logout } from '@/features/auth/Logout';
 
 export const Header = () => {
   return (
     <header className='bg-secondary flex items-center gap-6 justify-end py-2 pr-3'>
       <UserAvatar />
-      <ModeToggle />
+      <section className='flex gap-2'>
+        <ModeToggle />
+        <Logout />
+      </section>
     </header>
   );
 };
