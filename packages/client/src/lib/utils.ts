9cb9d798d0eaef24dc0e95a17779a03ad0aadf98
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-28 16:32:06 -0500

    Add type ReturnTypeLoader

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -1,5 +1,9 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+
+export type ReturnTypeLoader<T extends (...args: any) => any> = Awaited<
+ReturnType<ReturnType<T>>
+>;
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
