31d920445a4a35d14603f94479b47514a063aba8
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-09-29 16:06:24 -0500

    Refactor options into clientConfig variable

--- packages/client/src/config/trpc.ts
+++ packages/client/src/config/trpc.ts
@@ -7,7 +7,7 @@ import { AppRouter } from 'api-server';
 
 export const trpc = createTRPCReact<AppRouter>();
 
-export const trpcReactClient = trpc.createClient({
+const clientConfig = {
   links: [
     httpBatchLink({
       url: 'http://localhost:3000/api/v1/trpc',
@@ -23,24 +23,10 @@ export const trpcReactClient = trpc.createClient({
       },
     }),
   ],
-});
+};
 
-export const trpcVanillaClient = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: 'http://localhost:3000/api/v1/trpc',
-      /**
-       * Allows cookies to send cross-origin
-       * See: https://trpc.io/docs/client/cors
-       */
-      fetch(url, options) {
-        return fetch(url, {
-          ...options,
-          credentials: 'include',
-        });
-      },
-    }),
-  ],
-});
+export const trpcReactClient = trpc.createClient(clientConfig);
+
+export const trpcVanillaClient = createTRPCProxyClient<AppRouter>(clientConfig);
 
 export const TRPCProvider = trpc.Provider;
