a45f412ff6a4ab3e4486b83b951da2a8c5425f0f
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-01 21:47:35 -0500

    Create TransactionsList component

---
+++ packages/client/src/features/transactions/components/TransactionsList.tsx
@@ -0,0 +1,85 @@
+import { format } from 'date-fns';
+import { Prisma } from '@prisma/client';
+import { ColumnDef } from '@tanstack/react-table';
+import { useTransactions } from '@/features/transactions/hooks/useTransactions';
+import { CreateTransaction } from '@/features/transactions/components/CreateTransaction';
+import { DataTable } from '@/ui/data-table';
+import { TableCell } from '@/ui/table-cell';
+import { currency } from '@/lib/utils';
+import {
+  useCategories,
+  ICategoriesInTransactions,
+} from '@/features/categories/hooks/useCategories';
+
+const getTransactionColumns = (
+  categories: ICategoriesInTransactions
+): ColumnDef<Prisma.TransactionCreateManyInput>[] => [
+  {
+    accessorKey: 'date',
+    header: () => <TableCell>Date</TableCell>,
+    cell: ({ row }) => {
+      const displayDate = format(new Date(row.getValue('date')), 'PPP');
+      return <TableCell>{displayDate}</TableCell>;
+    },
+  },
+  {
+    accessorKey: 'contact',
+    header: () => <TableCell>Contact</TableCell>,
+    cell: ({ row }) => <TableCell>{row.getValue('contact')}</TableCell>,
+  },
+  {
+    accessorKey: 'description',
+    header: () => <TableCell>Description</TableCell>,
+    cell: ({ row }) => <TableCell>{row.getValue('description')}</TableCell>,
+  },
+  {
+    accessorKey: 'categoryId',
+    header: () => <TableCell>Category</TableCell>,
+    cell: ({ row }) => {
+      const currentCategoryId = row.getValue('categoryId');
+      const category = categories.find(
+        (category) => category.categoryId === currentCategoryId
+      );
+      return <TableCell>{category?.category.name}</TableCell>;
+    },
+  },
+  {
+    accessorKey: 'amount',
+    header: () => <TableCell>Amount</TableCell>,
+    cell: ({ row }) => {
+      const amount = currency(row.getValue('amount'));
+      return <TableCell className='font-medium'>{amount}</TableCell>;
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      return <TableCell>...</TableCell>;
+    },
+  },
+];
+
+interface ITransactionsListProps {
+  filters?: { accountId?: string; categoryId?: string };
+}
+
+export function TransactionsList({ filters }: ITransactionsListProps) {
+  const { isLoadingTransactions, transactions } = useTransactions(filters);
+  const { isLoadingCategories, categories } = useCategories({
+    transactionIds: transactions?.map((transaction) => transaction.id),
+  });
+
+  return (
+    <div className='flex flex-col gap-6'>
+      <DataTable
+        columns={getTransactionColumns(categories as ICategoriesInTransactions)}
+        data={transactions || []}
+        isLoading={isLoadingTransactions || isLoadingCategories}
+        noResultsText='No transactions.  Add one to get started!'
+      />
+      <div className='ml-auto'>
+        <CreateTransaction page='account' id={filters?.accountId as string} />
+      </div>
+    </div>
+  );
+}
