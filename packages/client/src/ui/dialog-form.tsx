5b8b0cfc5a33b8cc6b70c7d93deca98614741f9e
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-01 21:39:50 -0500

    Pass in any input component to DialogForm

--- packages/client/src/ui/dialog-form.tsx
+++ packages/client/src/ui/dialog-form.tsx
@@ -4,13 +4,16 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { forwardRef } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
+type ComponentWithProps<TProps> = (props: TProps) => JSX.Element;
+
 interface IInput {
   value: string;
   label: string;
+  component?: ComponentWithProps<any>;
   inputProps?: { [key: string]: any };
 }
 
-interface IAccountFormProps {
+interface IFormProps {
   inputs: IInput[];
   zodSchema: any;
   defaultValues: { [key: string]: any };
@@ -18,36 +21,39 @@ interface IAccountFormProps {
   disabled: boolean;
 }
 
-export const DialogForm = forwardRef<HTMLButtonElement, IAccountFormProps>(
+export const DialogForm = forwardRef<HTMLButtonElement, IFormProps>(
   ({ inputs, zodSchema, defaultValues, onSubmit, disabled }, submitBtnRef) => {
-    const accountForm = useForm({
+    const form = useForm({
       resolver: zodResolver(zodSchema),
       defaultValues,
     });
 
     return (
-      <Form form={accountForm}>
+      <Form form={form}>
         <form
           onSubmit={async (e) => {
             e.preventDefault();
-            await accountForm.handleSubmit(onSubmit)();
+            await form.handleSubmit(onSubmit, (err) => console.error(err))();
           }}
         >
           <fieldset disabled={disabled} className='flex flex-col gap-4'>
-            {inputs.map((input) => (
-              <Form.Field
-                key={input.value}
-                control={accountForm.control}
-                name={input.value}
-                render={({ field }) => (
-                  <FormItem
-                    label={input.label}
-                    field={field}
-                    inputProps={input.inputProps}
-                  />
-                )}
-              />
-            ))}
+            {inputs.map((input) => {
+              return (
+                <Form.Field
+                  key={input.value}
+                  control={form.control}
+                  name={input.value}
+                  render={({ field }) => (
+                    <FormItem
+                      label={input.label}
+                      field={field}
+                      inputProps={input.inputProps}
+                      inputComponent={input.component}
+                    />
+                  )}
+                />
+              );
+            })}
             <button ref={submitBtnRef} type='submit' className='hidden' />
           </fieldset>
         </form>
