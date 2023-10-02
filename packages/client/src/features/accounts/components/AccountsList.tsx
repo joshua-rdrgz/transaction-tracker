1a34c1b0651cba018f2b76c87478b4c56f302c87
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-01 21:46:23 -0500

    refactor to use custom TableCell component

--- packages/client/src/features/accounts/components/AccountsList.tsx
+++ packages/client/src/features/accounts/components/AccountsList.tsx
@@ -4,38 +4,35 @@ import { ColumnDef } from '@tanstack/react-table';
 import { Prisma } from '@prisma/client';
 import { AccountsDropdown } from '@/features/accounts/components/AccountsDropdown';
 import { CreateAccount } from '@/features/accounts/components/CreateAccount';
+import { TableCell } from '@/ui/table-cell';
+import { currency } from '@/lib/utils';
 
 const accountColumns: ColumnDef<Prisma.AccountCreateManyInput>[] = [
   {
     accessorKey: 'name',
-    header: () => <div className='text-right'>Account Name</div>,
-    cell: ({ row }) => <div className='text-right'>{row.getValue('name')}</div>,
+    header: () => <TableCell>Account Name</TableCell>,
+    cell: ({ row }) => <TableCell>{row.getValue('name')}</TableCell>,
   },
   {
     accessorKey: 'bank',
-    header: () => <div className='text-right'>Account Bank</div>,
-    cell: ({ row }) => <div className='text-right'>{row.getValue('bank')}</div>,
+    header: () => <TableCell>Account Bank</TableCell>,
+    cell: ({ row }) => <TableCell>{row.getValue('bank')}</TableCell>,
   },
   {
     accessorKey: 'balance',
-    header: () => <div className='text-right'>INITIAL Balance</div>,
+    header: () => <TableCell>INITIAL Balance</TableCell>,
     cell: ({ row }) => {
-      const balance = parseFloat(row.getValue('balance'));
-      const formatted = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(balance);
-
-      return <div className='text-right font-medium'>{formatted}</div>;
+      const balance = currency(row.getValue('balance'));
+      return <div className='text-right font-medium'>{balance}</div>;
     },
   },
   {
     id: 'actions',
     cell: ({ row }) => {
       return (
-        <div className='text-right'>
+        <TableCell>
           <AccountsDropdown accountId={row.original.id as string} />
-        </div>
+        </TableCell>
       );
     },
   },
