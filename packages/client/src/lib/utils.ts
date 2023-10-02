703ec32ba7c913474f5476e2cf1d9a727497fc89
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-01 21:34:33 -0500

    add currency util function

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -8,3 +8,13 @@ ReturnType<ReturnType<T>>
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function currency(num: number | string) {
+  const amount = +num;
+  const currency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount);
+
+  return currency;
+}
