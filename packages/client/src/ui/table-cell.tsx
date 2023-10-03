e7c6cd184087ac4ba94f3260c89c99764f77e1a4
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-03 13:30:18 -0500

    Add header/sorting styling

--- packages/client/src/ui/table-cell.tsx
+++ packages/client/src/ui/table-cell.tsx
@@ -1,13 +1,32 @@
 import { cn } from '@/lib/utils';
+import { Button } from '@/ui/button';
+import { ArrowUpDown } from 'lucide-react';
 
 interface ITableCellProps {
   children: React.ReactNode;
   className?: string;
+  isHeader?: boolean;
+  isSortable?: boolean;
+  onClickHeader?(): void;
 }
 
 export const TableCell: React.FC<ITableCellProps> = ({
   children,
   className,
+  isHeader,
+  isSortable,
+  onClickHeader,
 }) => {
-  return <div className={cn('text-right', className)}>{children}</div>;
+  if (isHeader)
+    return (
+      <Button
+        variant='ghost'
+        className={cn('hover:bg-muted', className)}
+        onClick={onClickHeader}
+      >
+        {children}
+        {isSortable && <ArrowUpDown className='ml-2 h-4 w-4' />}
+      </Button>
+    );
+  return <div className={cn('text-center', className)}>{children}</div>;
 };
