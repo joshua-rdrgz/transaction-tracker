5084dceec290fa3c81a2607323179b126ceaddf0
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-03 13:29:26 -0500

    add stylings for TableHead that have child button

--- packages/client/src/ui/data-table.tsx
+++ packages/client/src/ui/data-table.tsx
@@ -73,7 +73,10 @@ export function DataTable<TData, TValue>({
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead key={header.id}>
+                    <TableHead
+                      key={header.id}
+                      className='custom-class-has-button'
+                    >
                       {header.isPlaceholder
                         ? null
                         : flexRender(
