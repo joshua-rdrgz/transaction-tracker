d36badaf1156accc1473df6f1c2574118d094016
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-04 16:32:32 -0500

    format balance && initialBalance into hook return object

--- packages/client/src/features/accounts/hooks/useAccount.ts
+++ packages/client/src/features/accounts/hooks/useAccount.ts
@@ -1,13 +1,28 @@
-import { trpc } from '@/config/trpc';
 import { useLoaderData } from 'react-router-dom';
+import { trpc } from '@/config/trpc';
+import { IHookOptions } from '@/lib/types';
+import { AccountPageLoader } from '@/pages/loaders/accountPageLoader';
 
-export const useAccount = (accountId: string) => {
-  const initialData = useLoaderData();
+export type ReturnTypeUseAccount = ReturnType<typeof useAccount>['account'];
+
+export const useAccount = (
+  accountId: string,
+  options: IHookOptions = {
+    useReactRouterLoader: true,
+  }
+) => {
+  const [accountData, accountBalance] = useLoaderData() as AccountPageLoader;
 
   const { data: account } = trpc.accounts.readAccount.useQuery(accountId, {
-    // @ts-ignore (initialData will never be null)
-    initialData,
+    // @ts-ignore
+    initialData: options.useReactRouterLoader ? accountData : null,
   });
 
-  return { account };
+  return {
+    account: {
+      ...account,
+      balance: accountBalance,
+      initialBalance: account.balance,
+    },
+  };
 };
