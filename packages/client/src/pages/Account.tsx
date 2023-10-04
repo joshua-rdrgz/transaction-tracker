355bb2ed29b5cc17247839802078c8f049ce0024
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-04 18:19:06 -0500

    Reorganize page layout

--- packages/client/src/pages/Account.tsx
+++ packages/client/src/pages/Account.tsx
@@ -15,20 +15,22 @@ export default function AccountPage() {
       <h1 className='font-display text-3xl'>Account Information</h1>
       <div className='flex flex-col gap-10'>
         <AccountDetails account={account} />
-        <section className='flex flex-col gap-5 lg:flex-row max-w-full text-center'>
-          <ChartSection
-            title='Transactions From Account'
-            chart={<AccountTransactionsPieChart accountId={account.id} />}
-          />
-          <ChartSection
-            title='Transactions Over Time'
-            chart={<AccountTransactionsLineChart accountId={account.id} />}
-          />
-        </section>
-        <section>
-          <h2 className='font-display text-2xl'>Account Transactions</h2>
+        <div className='flex flex-col gap-10 lg:grid lg:grid-cols-2'>
+          <section className='flex flex-col gap-5 max-w-full text-center lg:col-start-2'>
+            <ChartSection
+              title='Transactions From Account'
+              chart={<AccountTransactionsPieChart accountId={account.id} />}
+            />
+            <ChartSection
+              title='Transactions Over Time'
+              chart={<AccountTransactionsLineChart accountId={account.id} />}
+            />
+          </section>
+          <section className='lg:col-start-1 lg:col-end-2 lg:row-start-1'>
+            <h2 className='font-display text-2xl'>Posted Transactions</h2>
             <TransactionsTable filters={{ accountId: account.id }} />
-        </section>
+          </section>
+        </div>
       </div>
     </section>
   );
--- packages/client/src/ui/chart-section.tsx
+++ packages/client/src/ui/chart-section.tsx
@@ -8,7 +8,7 @@ export const ChartSection: React.FC<IChartSectionProps> = ({
   chart,
 }) => {
   return (
-    <div className='flex-1 flex flex-col gap-2 bg-secondary p-3 rounded shadow-md dark:shadow-secondary lg:w-0'>
+    <div className='flex-1 flex flex-col gap-2 bg-secondary p-3 rounded shadow-md dark:shadow-secondary'>
       <h2 className='font-display text-2xl p-2'>{title}</h2>
       {chart}
     </div>
