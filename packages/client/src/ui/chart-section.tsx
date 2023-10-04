2edd9ceaa347c1b5acc957bfb15044f215bcb050
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-04 15:10:02 -0500

    Create ChartSection UI component

---
+++ packages/client/src/ui/chart-section.tsx
@@ -0,0 +1,16 @@
+interface IChartSectionProps {
+  title: string;
+  chart: React.ReactNode;
+}
+
+export const ChartSection: React.FC<IChartSectionProps> = ({
+  title,
+  chart,
+}) => {
+  return (
+    <div className='flex-1 flex flex-col gap-2 bg-secondary p-3 rounded shadow-md dark:shadow-secondary lg:w-0'>
+      <h2 className='font-display text-2xl p-2'>{title}</h2>
+      {chart}
+    </div>
+  );
+};
