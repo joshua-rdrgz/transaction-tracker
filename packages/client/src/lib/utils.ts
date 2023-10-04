c72b571fcf435517d4199994332aaffe45c8b009
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-04 16:31:34 -0500

    get rid of unneeded type util

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -1,9 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
-export type ReturnTypeLoader<T extends (...args: any) => any> = Awaited<
-ReturnType<ReturnType<T>>
->;
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
