40f0e04bfed6e01ad58755892dcca7b0a5cbaf99
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-10-08 00:40:35 -0500

    Add default values to UpdateTransactionForm

--- packages/client/src/features/transactions/forms/UpdateTransactionForm.tsx
+++ packages/client/src/features/transactions/forms/UpdateTransactionForm.tsx
@@ -4,11 +4,13 @@ import { toast } from 'react-hot-toast';
 import { z } from 'zod';
 import sharedZodSchemas from 'shared-zod-schemas';
 import { SelectCategory } from '@/features/categories/components/SelectCategory';
+import { useTransaction } from '@/features/transactions/hooks/useTransaction';
 import { useUpdateTransaction } from '@/features/transactions/hooks/useUpdateTransaction';
 import { SelectAccount } from '@/features/accounts/components/SelectAccount';
 import { DatePicker } from '@/ui/date-picker';
 import { DialogForm } from '@/ui/dialog-form';
 import { IDialogFormPropsReceivesTransaction } from '@/lib/types';
+import { Spinner } from '@/ui/spinner';
 
 const createTransactionSchema =
   sharedZodSchemas.transactionRouteSchemas.createTransaction;
@@ -54,6 +56,9 @@ export const UpdateTransactionForm = forwardRef<
   HTMLButtonElement,
   IDialogFormPropsReceivesTransaction
 >(({ setDialogOpen, setDropdownOpen, transactionId }, submitBtnRef) => {
+  const { isLoadingTransaction, transaction } = useTransaction(
+    transactionId || ''
+  );
   const { isUpdatingTransaction, updateTransaction } = useUpdateTransaction();
 
   const onSubmit = (values: FieldValues) => {
@@ -72,21 +77,28 @@ export const UpdateTransactionForm = forwardRef<
     );
   };
 
+  if (isLoadingTransaction)
+    return (
+      <div className='flex justify-center items-center h-72'>
+        <Spinner size={50} />
+      </div>
+    );
+
   return (
     <DialogForm
       ref={submitBtnRef}
       inputs={UPDATE_TRANSACTION_INPUTS}
       zodSchema={createTransactionSchema}
       defaultValues={{
-        date: '',
-        contact: '',
-        description: '',
-        category: '',
-        account: '',
-        amount: 0,
+        date: new Date(transaction?.date || Date.now()),
+        contact: transaction?.contact || '',
+        description: transaction?.description || '',
+        category: transaction?.categoryId,
+        account: transaction?.accountId || '',
+        amount: transaction?.amount || 0,
       }}
       onSubmit={onSubmit}
-      disabled={isUpdatingTransaction}
+      disabled={isLoadingTransaction || isUpdatingTransaction}
     />
   );
 });
